3152f353e28fb5fb23a62ba97a6ba68787b5dfe1
tanghaochen <haochentang@foxmail.com>
2023-08-06 16:10:31 +0800

    解决跨域问题

--- webook/main.go
+++ webook/main.go
@@ -5,14 +5,28 @@ import (
 	"github.com/TangTangHC/basic-go-study/webook/internal/repository/dao"
 	"github.com/TangTangHC/basic-go-study/webook/internal/service"
 	"github.com/TangTangHC/basic-go-study/webook/internal/web"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"strings"
+	"time"
 )
 
 func main() {
+	db := initDB()
+
 	server := gin.Default()
-	userHandler := initUserHandler(initDB())
+	server.Use(cors.New(cors.Config{
+		AllowMethods:     []string{"POST", "GET", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Authorization", "Content-Type"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+		AllowOriginFunc: func(origin string) bool {
+			return strings.HasPrefix(origin, "http://localhost")
+		},
+	}))
+	userHandler := initUserHandler(db)
 	userHandler.RegisterHandler(server)
 	err := server.Run(":8080")
 	if err != nil {
